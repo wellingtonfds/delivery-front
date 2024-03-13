fda345a069f61091e6ce3b761c6523aceb2b3c3a
wellington <wellingtonfds@gmail.com>
2024-03-13 20:25:00 -0300

    create cliente graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
+import createApolloClient from "@/graphql/client";
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-
+  const client = createApolloClient()
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <ApolloProvider client={client}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </ApolloProvider>
   )
 }
